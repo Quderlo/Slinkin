c3914538b7e5b21010b462d072713f088d20625d
Lev <60547547+Quderlo@users.noreply.github.com>
2025-05-27 17:02:42 +0500

    Update 339_v.c

--- sem_3-4/339_v.c
+++ sem_3-4/339_v.c
@@ -4,35 +4,36 @@ int main() {
     unsigned int m;
     char present;
     unsigned int result = 0;
-	
-    printf("Write len(a) m: ");
+
+    printf("Write len:");
     scanf("%u", &m);
-    
-    
+
+
     unsigned int a[m];
-    
+
     for (unsigned int i = 0; i < m; i++) {
-		printf("Write characters in a[%u]: ", i + 1);
+        printf("Write characters in a[%u]:", i + 1);
         scanf("%u", &a[i]);
     }
-    
-    
-    printf("Result: ");
-	for (unsigned int i = 0; i < m; i++) {
-		present = 0;
-		for (unsigned int j = 0; j < m; j++) {
-			if (a[i] == a[j] && i != j) {
-				present = 1;
-			}
-		}
-		if (!present)
-			result++;
-	}
-	
-	if (result)
-		printf("%d", result);
-	else
-		printf("None");
-    
+
+
+    for (unsigned int i = 0; i < m; ++i) {
+        present = 0;
+        for (unsigned int j = 0; j < i; ++j) {
+            if (a[i] == a[j]) {
+                present = 1;
+                break;
+            }
+        }
+        if (!present)
+            ++result;
+    }
+
+
+    if (result)
+        printf("%d", result);
+    else
+        printf("None");
+
     return 0;
 }
