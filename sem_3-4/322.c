aa0d003928011ea1684bd6dc2dc048d4054cf040
Lev <60547547+Quderlo@users.noreply.github.com>
2025-05-27 11:56:02 +0500

    Update 322.c

--- sem_3-4/322.c
+++ sem_3-4/322.c
@@ -7,18 +7,20 @@ int main() {
     unsigned int max_sum = 0;
     unsigned int max_number = 1;
     unsigned int sum;
-    
-    scanf("%u", &n);
-    
 
-    for (unsigned int i = sqrt(n); i <= n; i++) {
+    scanf("%u", &n);
+
+
+    for (unsigned int i = 1; i <= n; i++) {
         sum = 0;
-        for (unsigned int j = 1; j <= i / 2; j++)
-            if (i % j == 0)
-                sum += j;
-        
-        sum += i;
-        
+        for (unsigned int j = 1; j * j <= i; j++)
+            if (i % j == 0) {
+                if (j * j == i)
+                    sum += j;
+                else
+                    sum += j + i / j;
+            }
+
         if (sum > max_sum) {
             max_sum = sum;
             max_number = i;
