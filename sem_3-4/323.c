b7adef036c3386377a864d96594bf31c94d8c56b
Lev <60547547+Quderlo@users.noreply.github.com>
2025-05-27 15:00:41 +0500

    Update 323.c

--- sem_3-4/323.c
+++ sem_3-4/323.c
@@ -3,23 +3,30 @@
 
 int main() {
     unsigned int n;
-    char mutually_simple = 1;
+    unsigned int r;
+    unsigned int b;
+    unsigned int a;
+
 
     scanf("%u", &n);
-    
+
     for (unsigned int i = 1; i < n; i++) {
-        //printf("\n %u delitsy ", i);
-        mutually_simple = 1;
-        for (unsigned int j = 2; j <= sqrt(n); j++) {
-            if (i % j == 0 && n % j == 0) {
-				mutually_simple = 0;
-                break;
-            }
-       }
-		if (mutually_simple) {
-			printf("%u ", i);
+
+        a = i;
+        b = n;
+
+
+        while (a != 0) {
+            r = b % a;
+            b = a;
+            a = r;
         }
-        
+
+	# Если число простое то цикл закончится на и b = 1
+        if (b == 1) {
+            printf("%u ", i);
+        }
     }
+
     return 0;
 }
